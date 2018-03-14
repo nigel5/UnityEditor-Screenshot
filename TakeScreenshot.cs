488675a53506785fa4dc80d4aad4a262e516f735
Nigel <28766663+nigel5@users.noreply.github.com>
2018-03-13 21:41:32 -0400

    first commit

---
+++ TakeScreenshot.cs
@@ -0,0 +1,31 @@
+﻿//
+// MIT License
+//
+// Copyright (c) 2018 Nigel Huang
+// See LICENSE
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeScreenshot : MonoBehaviour
+{
+
+    public string filename = "screenshot";
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Screenshot service waiting... Right click to save screenshot to root folder of this project...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("took screenshot");
+            ScreenCapture.CaptureScreenshot(filename + ".png");
+        }
+    }
+}
